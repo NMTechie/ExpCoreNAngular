0d60b970de84a44925cb28386ace79c016879d49
Nilesh Ranjan Mitra <nileshranjan.mitra@gmail.com>
2019-06-18 11:29:49 +0000

    Step 2 - The Angular app is now integrated with MVC pipe line .Default MVC layout page is reference is moved out. Why the scripts need to go after the root component is a question ?

--- MVCProjectForExpAngular/src/app/app.component.ts
+++ MVCProjectForExpAngular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SampleAngularApp';
+  title = 'My Angular App With MVC';
 }
